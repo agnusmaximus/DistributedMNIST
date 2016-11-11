684ea2b88b1bff91ed48d9c46b2115a287e5b2c4
Max <agnusmaximus@berkeley.edu>
2016-11-11 00:38:03 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -141,7 +141,7 @@ def train(target, dataset, cluster_spec):
     total_loss = mnist.loss(logits, labels)
 
     # Create an optimizer that performs gradient descent.
-    optimizer = tf.train.MomentumOptimizer(learning_rate, 0.9).minimize(total_loss, global_step=global_step)
+    optimizer = tf.train.MomentumOptimizer(lr, 0.9).minimize(total_loss, global_step=global_step)
 
     # Use V2 optimizer
     opt = SyncReplicasOptimizerV2(
