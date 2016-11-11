5db0c8de3c17205a61d0b0112868ac4cfb05a083
Max <agnusmaximus@berkeley.edu>
2016-11-11 00:13:13 -0800

    Update

--- src/mnist.py
+++ src/mnist.py
@@ -41,24 +41,24 @@ def inference(images, hidden1_units=FLAGS.hidden1_units, hidden2_units=FLAGS.hid
   conv1_weights = tf.Variable(
       tf.truncated_normal([5, 5, NUM_CHANNELS, 32],  # 5x5 filter, depth 32.
                           stddev=0.1,
-                          seed=SEED, dtype=data_type()))
-  conv1_biases = tf.Variable(tf.zeros([32], dtype=data_type()))
+                          seed=SEED, dtype=tf.float32))
+  conv1_biases = tf.Variable(tf.zeros([32], dtype=tf.float32))
   conv2_weights = tf.Variable(tf.truncated_normal(
       [5, 5, 32, 64], stddev=0.1,
-      seed=SEED, dtype=data_type()))
-  conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=data_type()))
+      seed=SEED, dtype=tf.float32))
+  conv2_biases = tf.Variable(tf.constant(0.1, shape=[64], dtype=tf.float32))
   fc1_weights = tf.Variable(  # fully connected, depth 512.
       tf.truncated_normal([IMAGE_SIZE // 4 * IMAGE_SIZE // 4 * 64, 512],
                           stddev=0.1,
                           seed=SEED,
-                          dtype=data_type()))
-  fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=data_type()))
+                          dtype=tf.float32))
+  fc1_biases = tf.Variable(tf.constant(0.1, shape=[512], dtype=tf.float32))
   fc2_weights = tf.Variable(tf.truncated_normal([512, NUM_LABELS],
                                                 stddev=0.1,
                                                 seed=SEED,
-                                                dtype=data_type()))
+                                                dtype=tf.float32))
   fc2_biases = tf.Variable(tf.constant(
-      0.1, shape=[NUM_LABELS], dtype=data_type()))
+      0.1, shape=[NUM_LABELS], dtype=tf.float32))
 
   """The Model definition."""
   # 2D convolution, with 'SAME' padding (i.e. the output feature map has
