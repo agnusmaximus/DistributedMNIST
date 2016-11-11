e516f41cf6903e400ea6a284f144a3222212159b
Max <agnusmaximus@berkeley.edu>
2016-11-11 00:25:29 -0800

    Update

--- src/mnist_data.py
+++ src/mnist_data.py
@@ -26,4 +26,89 @@ import numpy
 from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
 import tensorflow as tf
-from tensorflow.contrib.learn.python.learn.datasets.mnist import load_mnist
+
+from tensorflow.contrib.learn.python.learn.datasets import base
+from tensorflow.python.framework import dtypes
+
+def extract_data(filename, num_images):
+  """Extract the images into a 4D tensor [image index, y, x, channels].
+  Values are rescaled from [0, 255] down to [-0.5, 0.5].
+  """
+  print('Extracting', filename)
+  with gzip.open(filename) as bytestream:
+    bytestream.read(16)
+    buf = bytestream.read(IMAGE_SIZE * IMAGE_SIZE * num_images)
+    data = numpy.frombuffer(buf, dtype=numpy.uint8).astype(numpy.float32)
+    data = (data - (PIXEL_DEPTH / 2.0)) / PIXEL_DEPTH
+    data = data.reshape(num_images, IMAGE_SIZE, IMAGE_SIZE, 1)
+    return data
+
+def extract_labels(filename, num_images):
+  """Extract the labels into a vector of int64 label IDs."""
+  print('Extracting', filename)
+  with gzip.open(filename) as bytestream:
+    bytestream.read(8)
+    buf = bytestream.read(1 * num_images)
+    labels = numpy.frombuffer(buf, dtype=numpy.uint8).astype(numpy.int64)
+  return labels
+
+def read_data_sets(train_dir,
+                   fake_data=False,
+                   one_hot=False,
+                   dtype=dtypes.float32,
+                   reshape=True,
+                   validation_size=5000):
+  if fake_data:
+
+    def fake():
+      return DataSet([], [], fake_data=True, one_hot=one_hot, dtype=dtype)
+
+    train = fake()
+    validation = fake()
+    test = fake()
+    return base.Datasets(train=train, validation=validation, test=test)
+
+  TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
+  TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
+  TEST_IMAGES = 't10k-images-idx3-ubyte.gz'
+  TEST_LABELS = 't10k-labels-idx1-ubyte.gz'
+
+  local_file = base.maybe_download(TRAIN_IMAGES, train_dir,
+                                   SOURCE_URL + TRAIN_IMAGES)
+  with open(local_file, 'rb') as f:
+    train_images = extract_data(f, 60000)
+
+  local_file = base.maybe_download(TRAIN_LABELS, train_dir,
+                                   SOURCE_URL + TRAIN_LABELS)
+  with open(local_file, 'rb') as f:
+    train_labels = extract_labels(f, 60000)
+
+  local_file = base.maybe_download(TEST_IMAGES, train_dir,
+                                   SOURCE_URL + TEST_IMAGES)
+  with open(local_file, 'rb') as f:
+    test_images = extract_data(f, 10000)
+
+  local_file = base.maybe_download(TEST_LABELS, train_dir,
+                                   SOURCE_URL + TEST_LABELS)
+  with open(local_file, 'rb') as f:
+    test_labels = extract_labels(f, 10000)
+
+  if not 0 <= validation_size <= len(train_images):
+    raise ValueError(
+        'Validation size should be between 0 and {}. Received: {}.'
+        .format(len(train_images), validation_size))
+
+  validation_images = test_images
+  validation_labels = test_labels
+  train_images = train_images
+  train_labels = train_labels
+
+  train = DataSet(train_images, train_labels, dtype=dtype, reshape=reshape)
+  validation = DataSet(validation_images,
+                            validation_labels,
+                            dtype=dtype,
+                            reshape=reshape)
+  return base.Datasets(train=train, validation=validation, test=None)
+
+def load_mnist(train_dir='MNIST-data'):
+  return read_data_sets(train_dir)
