7220a89a35b6d22d80ccaba1e60c0e898106186d
Max <agnusmaximus@berkeley.edu>
2016-11-11 13:31:13 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -135,7 +135,7 @@ def train(target, dataset, cluster_spec):
 
     # Number of classes in the Dataset label set plus 1.
     # Label 0 is reserved for an (unused) background class.
-    logits, reg = mnist.inference(images, train=True)
+    logits, reg = mnist.inference(images, train=False)
 
     # Add classification loss.
     #total_loss = mnist.loss(logits, labels) + reg
