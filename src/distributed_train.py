5a8f41c2fe747a7b5409ca217dbb0961867c3a8d
Max <agnusmaximus@berkeley.edu>
2016-11-10 21:36:56 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -196,7 +196,7 @@ def train(target, dataset, cluster_spec):
     local_global_step_init_op = tf.initialize_variables([local_global_step])
 
     # Build an initialization operation to run below.
-    init_op = [local_global_step_init_op, tf.initialize_all_variables()]
+    init_op = [tf.initialize_all_variables()]
 
     # We run the summaries in the same thread as the training operations by
     # passing in None for summary_op to avoid a summary_thread being started.
@@ -207,7 +207,7 @@ def train(target, dataset, cluster_spec):
     else:
       local_init_op = opt.local_step_init_op
 
-    local_init_opt = local_init_op
+    local_init_opt = [local_global_step_init_op, local_init_op]
     ready_for_local_init_op = opt.ready_for_local_init_op
 
     sv = tf.train.Supervisor(is_chief=is_chief,
