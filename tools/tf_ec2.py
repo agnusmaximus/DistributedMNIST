fc33efdf23c510df2fb2feb6a6f14240bf8f925e
Max <agnusmaximus@berkeley.edu>
2016-11-12 19:54:44 -0800

    Update

--- tools/tf_ec2.py
+++ tools/tf_ec2.py
@@ -42,7 +42,7 @@ configuration = Cfg({
     "worker_type" : "m4.2xlarge",
     "ps_type" : "m4.2xlarge",
     "evaluator_type" : "m4.2xlarge",
-    "image_id" : "ami-4749ea27",          # US west
+    "image_id" : "ami-56d97b36",          # US west
 
     # Launch specifications
     "spot_price" : ".12",                 # Has to be a string
