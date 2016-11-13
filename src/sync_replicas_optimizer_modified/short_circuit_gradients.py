57d18cb4b115dc4e12a0dc17d20876c60d8b0587
Max <agnusmaximus@berkeley.edu>
2016-11-13 10:27:19 -0800

    Update

--- src/sync_replicas_optimizer_modified/short_circuit_gradients.py
+++ src/sync_replicas_optimizer_modified/short_circuit_gradients.py
@@ -552,8 +552,7 @@ def gradients_short_circuited(ys,
             #new_global_step = logging_ops.Print(new_global_step, [new_global_step], message="CHECKING global step")
             prefetch_inputs = [tf.identity(x) for x in out_grads] + [tf.identity(x) for x in op.inputs]
             for kk in prefetch_inputs:
-              if "ps" in kk.device:
-                tf.logging.info("YOOO: %s" % kk.device)
+              tf.logging.info("YOOO: %s" % kk.device)
             in_grads = tf.cond(local_global_step >= 10000,
                                lambda : zero_grad_function(prefetch_inputs),
                                lambda : in_grad_function(prefetch_inputs))
