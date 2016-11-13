7ab29fcc1ad26389d18333fa7ae422efb55dffd9
Max <agnusmaximus@berkeley.edu>
2016-11-13 03:26:42 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -252,7 +252,7 @@ def train(target, dataset, cluster_spec):
         if FLAGS.timeline_logging:
           tl = timeline.Timeline(run_metadata.step_stats)
           ctf = tl.generate_chrome_trace_format()
-          with open('worker=%d_timeline_iter=%d.json' % (FLAGS.task_id, step), 'w') as f:
+          with open('%s/worker=%d_timeline_iter=%d.json' % (FLAGS.train_dir, FLAGS.task_id, step), 'w') as f:
             f.write(ctf)
 
         if step > FLAGS.max_steps:
