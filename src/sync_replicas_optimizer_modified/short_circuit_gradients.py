e638a1c71bc51c3b53a09611e3e64a2ce088271a
Max <agnusmaximus@berkeley.edu>
2016-11-13 11:03:32 -0800

    Update

--- src/sync_replicas_optimizer_modified/short_circuit_gradients.py
+++ src/sync_replicas_optimizer_modified/short_circuit_gradients.py
@@ -547,17 +547,12 @@ def gradients_short_circuited(ys,
 
         # If none gradient, no need to do anything
         if not none_gradient:
-          prefetch_inputs = [tf.identity(x) for x in out_grads] + [tf.identity(x) for x in op.inputs] + [tf.identity(x) for x in op.control_inputs]
           with ops.control_dependencies(out_grads):
             #new_global_step = tf.identity(global_step.ref())
             #new_global_step = logging_ops.Print(new_global_step, [new_global_step], message="CHECKING global step")
             #prefetch_inputs = out_grads + op.inputs + op.control_inputs
-            for kk in op.inputs:
-              tf.logging.info("Opinputs: %s, %s" % (kk.name, kk.device))
-            for kk in op.control_inputs:
-              tf.logging.info("Opcontrol: %s, %s" % (kk.name, kk.device))
-            for kk in out_grads:
-              tf.logging.info("Opout: %s, %s" % (kk.name, kk.device))
+            for i in range(len(op.inputs)):
+              op.inputs[i] = tf.identity(op.inputs[i])
             in_grads = tf.cond(local_global_step >= 10000,
                                lambda : zero_grad_function(prefetch_inputs),
                                lambda : in_grad_function(prefetch_inputs))
