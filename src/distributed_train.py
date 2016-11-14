c6918df27a24dd42165a2337ed555404ef3b1d3d
Max <agnusmaximus@berkeley.edu>
2016-11-14 14:33:31 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -139,14 +139,14 @@ class WorkerStatusServer(pb.Root):
 
   def remote_notify_starting(self, worker_id, iteration):
     # Called when worker_id notifies this machine that it is starting iteration.
-    tf.logging.info("Worker %d: Was notified that worker %d started iteration %d" % (self.worker_id, worker_id, iteration))
+    tf.logging.info("Worker %d: Was notified that worker %d started iteration %d - t=%d" % (self.worker_id, worker_id, iteration, int(time.time())))
     self.iteration_track[worker_id] = iteration
     self.check_is_straggler()
     return 0
 
   def remote_notify_finished(self, worker_id, iteration):
     # Called when worker_id notifies this machine that it finished a given iteration.
-    tf.logging.info("Worker %d: Was notified that worker %d finished iteration %d" % (self.worker_id, worker_id, iteration))
+    tf.logging.info("Worker %d: Was notified that worker %d finished iteration %d - t=%d" % (self.worker_id, worker_id, iteration, int(time.time())))
     self.iteration_finished[worker_id] = iteration
     return 0
 
@@ -217,7 +217,8 @@ class WorkerStatusClient:
     self.connected(perspective)
 
   def success(self, result):
-    tf.logging.info("Success!")
+    #tf.logging.info("Success!")
+    pass
 
   def fail(self, _):
     tf.logging.info("Fail")
