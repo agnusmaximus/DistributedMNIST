5f4b8c06dbf670e58f54c2d637f0f4f687dc9b1d
Max <agnusmaximus@berkeley.edu>
2016-11-13 20:25:19 -0800

    Update

--- src/sync_replicas_optimizer_modified/short_circuit_gradients.py
+++ src/sync_replicas_optimizer_modified/short_circuit_gradients.py
@@ -389,7 +389,7 @@ def gradients_short_circuited(ys,
     for op in to_ops + from_ops:
       for i,inp in enumerate(op.inputs):
         op._update_input(i, tf.identity(inp))
-      for inp in enumerate(op.inputs):
+      for inp in op.inputs:
         tf.logging.info("YAYO: %s" % inp.name)
 
     pending_count, loop_state = _PendingCount(ops.get_default_graph(),
