4d7f76e22aad395bfd30d7b17a0d3da99f7d8cfc
Max <agnusmaximus@berkeley.edu>
2016-11-14 15:38:17 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -146,7 +146,7 @@ class WorkerStatusServer(pb.Root):
 
   def remote_notify_finished(self, worker_id, iteration):
     # Called when worker_id notifies this machine that it finished a given iteration.
-    #tf.logging.info("Worker %d: Was notified that worker %d finished iteration %d - t=%f" % (self.worker_id, worker_id, iteration, time.time()))
+    tf.logging.info("Worker %d: Was notified that worker %d finished iteration %d - t=%f" % (self.worker_id, worker_id, iteration, time.time()))
     self.iteration_finished[worker_id] = iteration
     return 0
 
