ee165ecdea297a3f9e17c5c746739c2d1b9dac88
Max <agnusmaximus@berkeley.edu>
2016-11-13 22:41:11 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -280,6 +280,7 @@ def train(target, dataset, cluster_spec):
       except:
         if is_chief:
           tf.logging.info('About to execute sync_clean_up_op!')
+
         continue
 
     if is_chief:
