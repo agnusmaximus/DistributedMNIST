4667537a101aad86d7c0e6be1419936824d8852e
Max <agnusmaximus@berkeley.edu>
2016-11-14 17:44:17 -0800

    Update

--- src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
+++ src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
@@ -235,11 +235,11 @@ class SyncReplicasOptimizerV2(optimizer.Optimizer):
     Returns:
       A list of (gradient, variable) pairs.
     """
-    kwargs["sync_token_queue"] = self._sync_token_queue
-    kwargs["local_global_step"] = self._local_step
-    kwargs["global_step"] = self._global_step
-    return compute_gradients_with_injected_short_circuiting(*args, **kwargs)
-    #return self._opt.compute_gradients(*args, **kwargs)
+    #kwargs["sync_token_queue"] = self._sync_token_queue
+    #kwargs["local_global_step"] = self._local_step
+    #kwargs["global_step"] = self._global_step
+    #return compute_gradients_with_injected_short_circuiting(*args, **kwargs)
+    return self._opt.compute_gradients(*args, **kwargs)
 
   def apply_gradients(self, grads_and_vars, global_step=None, name=None):
     """Apply gradients to variables.
