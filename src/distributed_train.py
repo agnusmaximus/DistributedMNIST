4cb64534b63ded7c1db3dbaec33246d9715cadf4
Max <agnusmaximus@berkeley.edu>
2016-11-14 20:31:26 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -432,6 +432,7 @@ def train(target, dataset, cluster_spec):
         if is_chief:
           tf.logging.info('About to execute sync_clean_up_op!')
         sess.run(kill_cleanup_op)
+        continue
 
     if is_chief:
       tf.logging.info('Elapsed Time: %f' % (time.time()-begin_time))
