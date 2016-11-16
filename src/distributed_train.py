7f55d9b48e2ad81c73246a0a55288eeafe39c4f3
Max <agnusmaximus@berkeley.edu>
2016-11-15 19:14:33 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -432,10 +432,11 @@ def train(target, dataset, cluster_spec):
           # Determine the next time for running the summary.
           next_summary_time += FLAGS.save_summaries_secs
       except KeyboardInterrupt:
-        tf.logging.info("RECEIVED SIGNAL... CONTINUING")
+        tf.logging.info("RECEIVED SIGNAL... SIGINT")
       except:
         if is_chief:
           tf.logging.info('About to execute sync_clean_up_op!')
+        tf.logging.info("RECEIVED SIGNAL... NORMAL EXCEPTION")
         #sess.run(kill_cleanup_op)
 
     if is_chief:
