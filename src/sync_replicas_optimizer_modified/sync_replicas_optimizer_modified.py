f5e01c921fa6718a2b6bb372735e91094eab2fa4
Max <agnusmaximus@berkeley.edu>
2016-12-19 05:51:37 -0800

    Update

--- src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
+++ src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
@@ -359,12 +359,13 @@ class TimeoutReplicasOptimizer(optimizer.Optimizer):
 
         sync_ops = []
         with ops.control_dependencies([train_op]):
-          with ops.control_dependencies([update_op]):
-            with ops.control_dependencies([logging_ops.Print(global_step, [global_step], message="ENQUEING TO BEGIN NEXT ITER")]):
-              # Sync_op needs to insert tokens to the token queue at the end of the
-              # step so the replicas can fetch them to start the next step.
-              for worker in range(self._total_num_replicas):
-                sync_ops.append(self._sync_token_queues[worker].enqueue(global_step.ref()))
+          with ops.control_dependencies([logging_ops.Print(global_step, [global_step], message="Done training, time to try to update...")]):
+            with ops.control_dependencies([update_op]):
+              with ops.control_dependencies([logging_ops.Print(global_step, [global_step], message="ENQUEING TO BEGIN NEXT ITER")]):
+                # Sync_op needs to insert tokens to the token queue at the end of the
+                # step so the replicas can fetch them to start the next step.
+                for worker in range(self._total_num_replicas):
+                  sync_ops.append(self._sync_token_queues[worker].enqueue(global_step.ref()))
 
         self._chief_queue_runner = queue_runner.QueueRunner(dummy_queue,
                                                             [control_flow_ops.group(*(sync_ops))])
@@ -373,7 +374,7 @@ class TimeoutReplicasOptimizer(optimizer.Optimizer):
           chief_init_ops.append(
               accum.set_global_step(
                   global_step, name="SetGlobalStep"))"""
-      self.chief_init_op = self.local_step_init_op#control_flow_ops.group(*(chief_init_ops))
+      self.chief_init_op = control_flow_ops.group(*(chief_init_ops))
       self._gradients_applied = True
       return train_op
 
