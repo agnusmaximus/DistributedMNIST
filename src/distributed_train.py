97aaddb7620f9ffcdd347acaf9eb439186c4a431
Max <agnusmaximus@berkeley.edu>
2016-12-19 02:40:35 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -328,6 +328,8 @@ def train(target, dataset, cluster_spec):
     grads = opt.compute_gradients(total_loss)
     #apply_gradients_op, kill_cleanup_op = opt.minimize(total_loss, global_step=global_step)
     #apply_gradients_op = opt.minimize(total_loss, global_step=global_step)
+    tf.logging.info("YAYAYAY")
+    tf.logging.info(global_step)
     apply_gradients_op = opt.apply_gradients(grads, FLAGS.task_id, global_step=global_step)
 
     with tf.control_dependencies([apply_gradients_op]):
