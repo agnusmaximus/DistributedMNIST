4504369b0d0a902d3201bb27e4b93710ea44d305
Max <agnusmaximus@berkeley.edu>
2016-12-19 00:06:42 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -281,7 +281,7 @@ def train(target, dataset, cluster_spec):
     # number of updates applied to the variables. The PS holds the global step.
     global_step = tf.Variable(0, name="global_step", trainable=False)
     tf.logging.info("YOOOO")
-    tf.logging.info(global_step.device))
+    tf.logging.info(global_step.device)
 
     # Calculate the learning rate schedule.
     num_batches_per_epoch = (dataset.num_examples / FLAGS.batch_size)
