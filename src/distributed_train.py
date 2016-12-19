ab044a38ad47682cc1475ce9e6bcb3bd7fe0085c
Max <agnusmaximus@berkeley.edu>
2016-12-19 06:08:47 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -406,6 +406,7 @@ def train(target, dataset, cluster_spec):
           rpc_client.broadcast_starting(cur_iteration)
         start_time = time.time()
         feed_dict = mnist.fill_feed_dict(dataset, images, labels, FLAGS.batch_size)
+        tf.logging.info(feed_dict)
 
         if FLAGS.timeline_logging:
           run_options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
