03d5ca186ae1e2cd2cb9d2afafd3eaf844674871
Max <agnusmaximus@berkeley.edu>
2016-12-19 02:08:43 -0800

    Update

--- src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
+++ src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
@@ -445,7 +445,7 @@ class TimeoutReplicasOptimizer(optimizer.Optimizer):
                               self._global_step.ref())
       for i in range(self._total_num_replicas):
         with ops.control_dependencies([logging_ops.Print(self._global_step.ref(), [self._global_step.ref()], message="Init token queue")]):
-          init_tokens_op = self._sync_token_queues[i].enqueue(self._global_step.ref()))
+          init_tokens_op = self._sync_token_queues[i].enqueue(self._global_step.ref())
         init_tokens.append(init_tokens_op)
 
     return init_tokens
