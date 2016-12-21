b65896578dbac5405efa78d907e7530aba8cc2ef
Max <agnusmaximus@berkeley.edu>
2016-12-20 17:47:32 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -173,7 +173,6 @@ class WorkerStatusServer(pb.Root):
   # If so, we kill self.
   # Assumes that we have just started the current iteration.
   def set_suicide_timeout(self, iter_start_time, cur_iteration):
-    return
 
     # If there is not enough data to set a good timeout, continue
     if cur_iteration < self.iteration_end_collect:
