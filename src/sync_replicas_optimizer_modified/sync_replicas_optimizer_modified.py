46b43ea9f83349b7b0d64182005a2379034d18ca
Max <agnusmaximus@berkeley.edu>
2016-12-22 01:55:49 -0800

    Update

--- src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
+++ src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
@@ -320,9 +320,9 @@ class TimeoutReplicasOptimizer(optimizer.Optimizer):
       # a token that is >= the current global step
       finished_phase_1 = []
       for i in range(self._total_num_replicas):
-        dequeue = tf.while_loop(lambda x : tf.less(self._p1_finished_queues[i].dequeue(), x),
+        dequeue = tf.while_loop(lambda x : tf.less(self._p1_finished_queues[i].dequeue(), global_step.ref()),
                                 lambda x : x,
-                                [global_step.ref()])
+                                [global_step])
         finished_phase_1.append(dequeue)
       finished_phase_1 = control_flow_ops.group(*(finished_phase_1))
 
