c08341cf0b77c8d9b148466fd9502ca16be01b86
Max <agnusmaximus@berkeley.edu>
2016-12-22 02:03:41 -0800

    Update

--- src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
+++ src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
@@ -374,7 +374,8 @@ class TimeoutReplicasOptimizer(optimizer.Optimizer):
             # step so the replicas can fetch them to start the next step.
             #sync_ops.append(logging_ops.Print(global_step, [global_step], message="ENQUEING TO BEGIN NEXT ITER"))
             for worker in range(self._total_num_replicas):
-              enqueue_op = self._sync_token_queues[worker].enqueue(global_step.ref())
+              with ops.control_dependencies([tf.Assert(tf.equal(self._sync_token_queues[worker].size(), 0))]):
+                enqueue_op = self._sync_token_queues[worker].enqueue(global_step.ref())
               sync_ops.append(enqueue_op)
 
         self._chief_queue_runner = queue_runner.QueueRunner(dummy_queue,
