3286df2ce93303e9f58fb75a392b28fa6ab07d6e
Max <agnusmaximus@berkeley.edu>
2016-12-22 21:37:50 -0800

    Update for new tensorflow version

--- src/timeout_manager.py
+++ src/timeout_manager.py
@@ -34,7 +34,7 @@ class TimeoutServer(pb.Root):
     self.ITERATION_TO_BEGIN_STATISTICS_COLLECTION = 10
     self.ITERATION_TO_END_STATISTICS_COLLECTION = 80
     self.ITERATION_TO_START_TIMEOUT = 80
-    self.HIST_PERCENTILE = 5
+    self.HIST_PERCENTILE = 90
 
   def set_timeout(self, iteration):
     if iteration >= self.ITERATION_TO_START_TIMEOUT:
@@ -53,6 +53,7 @@ class TimeoutServer(pb.Root):
         tf.logging.info(sorted(histogram))
         self.timeout = int(np.percentile(histogram, self.HIST_PERCENTILE) * 1000)
         tf.logging.info("Timeout @ %f percentile = %d ms" % (self.HIST_PERCENTILE, self.timeout))
+        self.timeout = 50
 
   # Keep track of statistics of iterations start times
   def track_worker_start_times(self, worker_id, iteration, time):
