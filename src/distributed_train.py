5d1138972105e42ea2f9e8ad31b5c6612853da29
Max <agnusmaximus@berkeley.edu>
2016-12-25 06:53:14 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -293,7 +293,7 @@ def train(target, dataset, cluster_spec):
           run_metadata = tf.RunMetadata()
           loss_value, step = sess.run([train_op, global_step], options=run_options, run_metadata=run_metadata, feed_dict=feed_dict)
         else:
-          if timeout_server.timeout < 0 or FLAGS.task_id == 1:
+          if (timeout_server.timeout < 0 or FLAGS.task_id == 1) or not FLAGS.timeout_method:
             loss_value, step = sess.run([train_op, global_step], feed_dict=feed_dict)
           else:
             tf.logging.info("Setting timeout: %d ms" % timeout_server.timeout)
