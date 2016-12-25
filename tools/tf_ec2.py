b41ac885d0a083ac132d0ec647027c1e57c72065
Max <agnusmaximus@berkeley.edu>
2016-12-25 08:08:38 -0800

    Update

--- tools/tf_ec2.py
+++ tools/tf_ec2.py
@@ -29,10 +29,10 @@ configuration = Cfg({
 
     # Cluster topology
     "n_masters" : 1,                      # Should always be 1
-    "n_workers" : 5,
-    "n_ps" : 2,
+    "n_workers" : 19,
+    "n_ps" : 4,
     "n_evaluators" : 1,                   # Continually validates the model on the validation data
-    "num_replicas_to_aggregate" : "25",
+    "num_replicas_to_aggregate" : "20",
 
     # Region speficiation
     "region" : "us-west-2",
