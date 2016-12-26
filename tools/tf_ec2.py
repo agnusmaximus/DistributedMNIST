2d8c269fe89a03b96c4206f0df379d2433a38e66
Max <agnusmaximus@berkeley.edu>
2016-12-26 05:31:26 -0800

    Update

--- tools/tf_ec2.py
+++ tools/tf_ec2.py
@@ -79,7 +79,7 @@ cfg = Cfg({
 
     # Model configuration
     "batch_size" : "128",
-    "initial_learning_rate" : ".08",
+    "initial_learning_rate" : ".04",
     "learning_rate_decay_factor" : ".95",
     "num_epochs_per_decay" : "1.0",
 
