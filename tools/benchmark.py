c0321f11e35adfc45e81eef5cd6465c9f9760dac
Max <agnusmaximus@berkeley.edu>
2016-12-27 21:26:51 -0800

    Update benchmark, add cfg for 50 workers, 1024 batch size

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import sys
 import time
+import numpy as np
 import re
 import shutil
 import os
@@ -30,48 +31,78 @@ def run_tf_and_download_evaluator_file(run_time_sec, cfg, evaluator_file_name="o
 
     time.sleep(run_time_sec)
 
+    tf_ec2_run(kill_args.split(), cfg)
+
+    time.sleep(10)
+
     download_evaluator_file_args = "tools/tf_ec2.py download_file %s %s %s" % (cluster_string, evaluator_file_name, outdir)
     tf_ec2_run(download_evaluator_file_args.split(), cfg)
 
 def extract_times_losses_precision(fname):
     f = open(fname)
 
-    times, losses, precisions = [], [], []
+    times, losses, precisions, steps = [], [], [], []
     for line in f:
         m = re.match("Num examples: ([0-9]*)  Precision @ 1: ([\.0-9]*) Loss: ([\.0-9]*) Time: ([\.0-9]*)", line)
+        step_match = re.match(".* step=([0-9]*)", line)
         if m:
             examples, precision, loss, time = int(m.group(1)), float(m.group(2)), float(m.group(3)), float(m.group(4))
             times.append(time)
             losses.append(loss)
             precisions.append(precision)
+        if step_match:
+            step = int(step_match.group(1))
+            steps.append(step)
     f.close()
-    return times, losses, precisions
+    min_length = min([len(x) for x in [times, losses, precisions, steps]])
+    return times[:min_length], losses[:min_length], precisions[:min_length], steps[:min_length]
 
-def plot_time_loss(cfgs, evaluator_file_name="out_evaluator", outdir="result_dir", time_limit=350, rerun=False, launch=False):
+def plot_time_loss(outdir):
+    plt.cla()
+    plt.xlabel("time (s)")
+    plt.ylabel("loss")
+    files = glob.glob(outdir + "/*")
+    cmap = plt.get_cmap('jet')
+    colors = cmap(np.linspace(0, 1.0, len(files)))
+    for i, fname in enumerate(files):
+        label = fname.split("/")[-1]
+        times, losses, precisions, steps = extract_times_losses_precision(fname)
+        print(times, losses, precisions, steps)
+        plt.plot(times, losses, linestyle='solid',  marker='o', label=label, color=colors[i])
+    plt.legend(loc="upper right", fontsize=5)
+    plt.savefig("time_loss.png")
+
+def plot_time_step(outdir):
+    plt.cla()
+    plt.xlabel("time (s)")
+    plt.ylabel("step")
+    files = glob.glob(outdir + "/*")
+    cmap = plt.get_cmap('jet')
+    colors = cmap(np.linspace(0, 1.0, len(files)))
+    for i, fname in enumerate(files):
+        label = fname.split("/")[-1]
+        times, losses, precisions, steps = extract_times_losses_precision(fname)
+        print(times, losses, precisions, steps)
+        plt.plot(times, steps, linestyle='solid',  marker='o', label=label, color=colors[i])
+    plt.legend(loc="upper left", fontsize=5)
+    plt.savefig("time_step.png")
+
+def plot_figs(cfgs, evaluator_file_name="out_evaluator", outdir="result_dir", time_limit=350, rerun=False, launch=False):
 
     if rerun:
-
-        if os.path.exists(outdir):
-            shutil.rmtree(outdir)
-        os.makedirs(outdir)
-
         if launch:
+            if os.path.exists(outdir):
+                shutil.rmtree(outdir)
+                os.makedirs(outdir)
             shutdown_and_launch(cfgs[0])
         for cfg in cfgs:
             run_tf_and_download_evaluator_file(time_limit, cfg, evaluator_file_name=evaluator_file_name, outdir=outdir)
 
-    plt.xlabel("time (s)")
-    plt.ylabel("loss")
-    for fname in glob.glob(outdir + "/*"):
-        label = fname.split("/")[-1]
-        times, losses, precisions = extract_times_losses_precision(fname)
-        print(times, losses, precisions)
-        plt.plot(times, losses, linestyle='solid',  marker='o', label=label)
-    plt.legend(loc="upper right", fontsize=8)
-    plt.savefig("time_loss.png")
+    plot_time_loss(outdir)
+    plot_time_step(outdir)
 
 if __name__ == "__main__":
     print("Usage: python benchmark.py config_file1 config_file2")
     cfgs = [str(x) for x in sys.argv[1:]]
     cfgs = [load_cfg_from_file(x) for x in cfgs]
-    plot_time_loss(cfgs)
+    plot_figs(cfgs)
