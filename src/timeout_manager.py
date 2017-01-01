ed6a30b9bae032585a89f88066625194d8d4d829
Max <agnusmaximus@berkeley.edu>
2016-12-31 21:24:54 -0800

    token_queues to sync_token_queues

--- src/timeout_manager.py
+++ src/timeout_manager.py
@@ -38,7 +38,8 @@ class TimeoutServer(pb.Root):
 
     elapsed_time = self.worker_finished_computing_gradients_times[worker_id][iteration] - self.worker_dequeue_times[worker_id][iteration]
     self.compute_times.append((elapsed_time, iteration))
-    tf.logging.info("Compute times", [x[0] for x in self.compute_times])
+    tf.logging.info("COMPUTE TIMES")
+    tf.logging.info([x[0] for x in self.compute_times])
 
   def remote_notify_ready_to_start(self):
     tf.logging.info("Server ready to start!")
