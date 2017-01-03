91473909037f29108703d99bbfd86620736d5e13
Max <agnusmaximus@berkeley.edu>
2017-01-03 13:51:21 -0800

    Fix

--- src/timeout_manager.py
+++ src/timeout_manager.py
@@ -63,13 +63,13 @@ class TimeoutServer(pb.Root):
 
 class EchoClientFactory(pb.PBClientFactory):
     def startedConnecting(self, connector):
-        tf.logging.info 'Started to connect.'
+        tf.logging.info('Started to connect.')
 
     def clientConnectionLost(self, connector, reason):
-        tf.logging.info 'Lost connection.  Reason:', reason
+        tf.logging.info('Lost connection.  Reason:', reason)
 
     def clientConnectionFailed(self, connector, reason):
-        tf.logging.info 'Connection failed. Reason:', reason
+        tf.logging.info('Connection failed. Reason:', reason)
         time.sleep(5)
         connector.connect()
 
