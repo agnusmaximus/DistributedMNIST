a4b7483dd523dd630d4b86704cc8531a2f76a20b
Max <agnusmaximus@berkeley.edu>
2017-01-03 21:59:14 -0800

    Fix

--- src/timeout_manager.py
+++ src/timeout_manager.py
@@ -82,6 +82,7 @@ class TimeoutReconnectClientFactory(pb.PBClientFactory):
         return RetryTimeoutProtocol()
 
     def startedConnecting(self, connector):
+        tf.logging.info("cONNECTOR: %s" % str(type(connector)))
         tf.logging.info('Started to connect.')
 
     def clientConnectionLost(self, connector, reason):
