989f95922b243a75d80158a8d0a9cea79d84f58e
Max <agnusmaximus@berkeley.edu>
2017-01-09 05:17:34 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -291,6 +291,8 @@ def train(target, dataset, cluster_spec):
           run_options.trace_level=tf.RunOptions.FULL_TRACE
           run_options.output_partition_graphs=True
 
+        run_options.timeout_in_ms = 1000
+
         tf.logging.info("RUNNING SESSION...")
         loss_value, step = sess.run([train_op, global_step], feed_dict=feed_dict, run_metadata=run_metadata, options=run_options)
         tf.logging.info("DONE RUNNING SESSION...")
