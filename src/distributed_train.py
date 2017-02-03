aafc9b6f00336fc903a13a4bf016890ea2636edb
Max <agnusmaximus@berkeley.edu>
2017-02-03 00:08:11 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -165,7 +165,7 @@ def train(target, cluster_spec):
 
     # Compute gradients with respect to the loss.
     grads = opt.compute_gradients(total_loss)
-    apply_gradients_op, dequeue_op, print_end_op = opt.apply_gradients(grads, global_step=global_step)
+    apply_gradients_op = opt.apply_gradients(grads, FLAGS.task_id, global_step=global_step)
 
     with tf.control_dependencies([apply_gradients_op]):
       train_op = tf.identity(total_loss, name='train_op')
