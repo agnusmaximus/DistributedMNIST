40eb7dffe730591191d7df01074bad3e96e848db
dongyin92 <yindong10@gmail.com>
2017-02-09 17:27:12 -0800

    Update distributed_train.py

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -314,6 +314,7 @@ def train(target, dataset, cluster_spec):
           # Determine the next time for running the summary.
           next_summary_time += FLAGS.save_summaries_secs
       except:
+        a = 1
       #  tf.logging.info("Unexpected error: %s" % str(sys.exc_info()[0]))
       #  raise
 
