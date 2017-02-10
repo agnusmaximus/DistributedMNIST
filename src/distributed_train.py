051e033ab938e41ae99e36a6d3724fcf20760a1b
dongyin92 <yindong10@gmail.com>
2017-02-09 17:57:07 -0800

    Update distributed_train.py

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -261,6 +261,7 @@ def train(target, dataset, cluster_spec):
           test_batch_size = 5000
           n_rounds = n_example / test_batch_size
           cum_acc = 0.0   
+          tf.logging.info('before for loop')
           for round in xrange(n_rounds):
             tf.logging.info('before placeholder_inputs')
             images_placeholder, labels_placeholder = mnist.placeholder_inputs(test_batch_size)
