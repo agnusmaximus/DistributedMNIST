68df604a746a37346e0ca453f3551964cc7c23f7
dongyin92 <yindong10@gmail.com>
2017-02-09 21:13:34 -0800

    Update distributed_train.py

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -274,6 +274,7 @@ def train(target, dataset, cluster_spec):
                         labels_placeholder,
                         test_batch_size)
           round_acc = sess.run([validation_accuracy], feed_dict=feed_dict)
+          tf.logging.info('round_acc = %f' % round_acc)
           cum_acc += round_acc
           
         tf.logging.info('after for loop')
