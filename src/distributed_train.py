0d8623433121f5e10224614aab7e0f410a9d966f
Max <agnusmaximus@berkeley.edu>
2017-02-21 03:25:51 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -307,11 +307,11 @@ def train(target, cluster_spec):
 
       # Compute batchsize ratio
       if FLAGS.variable_batchsize_r:
-        new_epoch = int(math.round(n_examples_processed / cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN))
+        new_epoch = int(round(n_examples_processed / cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN))
         if n_examples_processed == 0 or new_epoch > cur_epoch:
           R = compute_R(sess, grads_and_vars_R, images_R, labels_R, images, labels)
 
-      cur_epoch = max(cur_epoch, int(math.round(n_examples_processed / cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN)))
+      cur_epoch = max(cur_epoch, int(round(n_examples_processed / cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN)))
 
       sess.run([opt._wait_op])
       timeout_client.broadcast_worker_dequeued_token(cur_iteration)
