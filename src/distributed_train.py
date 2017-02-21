2517636757e6c523089cf3b40cf725d401175e5f
Max <agnusmaximus@berkeley.edu>
2017-02-21 07:54:43 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -126,8 +126,8 @@ def compute_R(sess, grads_and_vars, images_R, labels_R, images_pl, labels_pl):
 
     gradients = sess.run([x[0] for x in grads_and_vars], feed_dict=feed_dict)
     gradient = np.concatenate(np.array([x.flatten() for x in gradients]))
-    tf.logging.info("YO %f" % np.linalg.norm(gradient))
     gradient *= FLAGS.batch_size
+    tf.logging.info("YO %f" % np.linalg.norm(gradient))
 
     if sum_of_norms == None:
       sum_of_norms = np.linalg.norm(gradient)**2
