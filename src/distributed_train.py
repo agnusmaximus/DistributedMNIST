3180b976abf4eee4772a45a905ef120bae2790de
Max <agnusmaximus@berkeley.edu>
2017-02-21 00:29:56 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -270,8 +270,7 @@ def train(target, cluster_spec):
         run_options.output_partition_graphs=True
 
       # We dequeue images form the shuffle queue
-      #images_real, labels_real = sess.run([dequeue_inputs[i]])
-      k = sess.run([dequeue_inputs[0]])
+      images_real, labels_real = sess.run([dequeue_inputs[0]])
       tf.logging.info("YOOOO")
       tf.logging.info(k)
 
