498a881becc5d0eb1e07bad3eec5020f0700a6af
Max <agnusmaximus@berkeley.edu>
2017-02-24 16:38:19 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -386,7 +386,7 @@ def train(target, cluster_spec):
 
       # We dequeue images form the shuffle queue
       if FLAGS.variable_batchsize_r:
-        batchsize_to_use = min(1023, int(R / 2 / num_workers))
+        batchsize_to_use = min(1023, int(R / 5 / num_workers))
         batchsize_to_use = int(int(batchsize_to_use / 32) * 32)
         tf.logging.info("Overall batchsize %f, worker batchsize %d" % (R, batchsize_to_use))
         images_real, labels_real = sess.run(dequeue_inputs[batchsize_to_use-1])
