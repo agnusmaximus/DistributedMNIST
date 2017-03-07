f46179baa193110ade79d27d72f4347eeb840a24
Max <agnusmaximus@berkeley.edu>
2017-03-06 18:57:20 -0800

    Update

--- src/cifar10_eval.py
+++ src/cifar10_eval.py
@@ -48,7 +48,7 @@ FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('eval_dir', '/tmp/cifar10_eval',
                            """Directory where to write event logs.""")
-tf.app.flags.DEFINE_string('eval_data', 'train_eval',
+tf.app.flags.DEFINE_string('eval_data', 'test',
                            """Either 'test' or 'train_eval'.""")
 tf.app.flags.DEFINE_string('checkpoint_dir', '/tmp/cifar10_train',
                            """Directory where to read model checkpoints.""")
