aee5c66d66b39d288d958d0fc31c63e6fe09eb8b
Max <agnusmaximus@berkeley.edu>
2017-03-08 23:27:07 -0800

    Update

--- src/cifar_input.py
+++ src/cifar_input.py
@@ -79,14 +79,21 @@ def build_input(dataset, data_path, batch_size, mode):
     label_bytes = 1
     label_offset = 0
     num_classes = 10
-    data_path += "/cifar-10-batches-bin/*"
+    data_path += "/cifar-10-batches-bin/"
   elif dataset == 'cifar100':
     label_bytes = 1
     label_offset = 1
     num_classes = 100
-    data_path += "/cifar-100-binary/*"
+    data_path += "/cifar-100-binary/"
   else:
     raise ValueError('Not supported dataset %s', dataset)
+
+  if mode == "train":
+    data_path += "data_batch*"
+  elif mode == "test":
+    data_path += "test_batch.bin"
+  else:
+    raise ValueError('Not supported mode %s', mode)
 
   depth = 3
   image_bytes = image_size * image_size * depth
