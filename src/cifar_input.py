96478b4fe8ac4ccf1b0ae9458f6390ffdd08baef
Max <agnusmaximus@berkeley.edu>
2017-03-09 16:51:36 -0800

    Update

--- src/cifar_input.py
+++ src/cifar_input.py
@@ -30,7 +30,7 @@ FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar_data',
                            """Path to the CIFAR-10 data directory.""")
-tf.app.flags.DEFINE_string('dataset', 'cifar10',
+tf.app.flags.DEFINE_string('dataset', 'cifar100',
                            """cifar10 or cifar100""")
 tf.app.flags.DEFINE_integer('batch_size', 128,
                             """Number of images to process in a batch.""")
