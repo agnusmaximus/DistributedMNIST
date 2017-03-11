41c5d0452de63a49f15fed95862285b564c62c47
Max <agnusmaximus@berkeley.edu>
2017-03-11 01:06:15 -0800

    Update

--- src/distributed_train.py
+++ src/distributed_train.py
@@ -284,7 +284,7 @@ def train(target, cluster_spec):
           R = compute_R(mon_sess, grads, variable_batchsize_inputs[1000], images, labels, 1000)
           mon_sess.run([R_enqueue_op], feed_dict={R_placeholder : R, images:np.zeros([1, 32, 32, 3]), labels: np.zeros([1, 10 if FLAGS.dataset == 'cifar10' else 100])})
           tf.logging.info("Master computed R - %f" % float(R))
-        R = mon_sess.run([R_dequeue_op])[0]
+        R = mon_sess.run([R_dequeue_op], feed_dict={images:np.zeros([1, 32, 32, 3]), labels: np.zeros([1, 10 if FLAGS.dataset == 'cifar10' else 100])})[0]
 
       # Dequeue variable batchsize inputs
       batchsize_to_use = R if FLAGS.variable_batchsize else FLAGS.batch_size
